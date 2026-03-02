b494a62e0a4365e79129dec1796ecb77b3059e18
Nicolly Ribeiro Luz <nicolly.luz@ufu.br>
2026-03-02 12:45:01 -0300

    Add files via upload

---
+++ Workshop2/app.py
@@ -0,0 +1,9 @@
+import pyfiglet
+import time
+
+print("Iniciando o sistema de produção...")
+time.sleep(2)
+mensagem = pyfiglet.figlet_format("VOCE - CONSEGUIU!")
+print("Carregando dependências...")
+time.sleep(1)
+print(mensagem)
